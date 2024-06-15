d331c9a1eb66099d749eaf12f953985134d84c15
stevenmoore <stevenmoore@macbook-pro.localdomain>
2024-06-15 11:34:20 -0500

    Update BankAccount

--- CSC372/src/BankAccount.java
+++ CSC372/src/BankAccount.java
@@ -1,5 +1,15 @@
 
 public class BankAccount {
+	// initialize fields
+	private String firstName;
+	private String lastName;
+	private int accountID;
+	private double balance;
+	
+	// Constructor that sets balance to 0
+	public BankAccount() {
+		balance = 0;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
