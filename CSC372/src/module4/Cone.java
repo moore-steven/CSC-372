1e435c7c415fcdd49c30d7b1e894efe07085e520
moore-steven <steven.moore@csuglobal.edu>
2024-07-04 13:40:26 -0500

    Completed Cone Class

---
+++ CSC372/src/module4/Cone.java
@@ -0,0 +1,26 @@
+package module4;
+
+public class Cone extends Shape {
+	private double radius;
+	private double height;
+
+	public Cone(double radius, double height) {
+		this.radius = radius;
+		this.height = height;
+	}
+
+	@Override
+	double surface_area() {
+		return Math.PI*radius*(radius+Math.sqrt(Math.pow(height, 2)+Math.pow(radius, 2)));
+	}
+
+	@Override
+	double volume() {
+		return Math.PI*Math.pow(radius, 2)*(height/3);
+	}
+	
+	public String toString() {
+		return "Volume: " + volume() + " Surface Area: " + surface_area();
+	}
+
+}
