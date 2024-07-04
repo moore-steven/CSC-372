30154ae1e1b08d1251625077cc1847b47e109982
moore-steven <steven.moore@csuglobal.edu>
2024-07-04 12:43:42 -0500

    Completed Sphere class

---
+++ CSC372/src/module4/Sphere.java
@@ -0,0 +1,22 @@
+package module4;
+
+
+public class Sphere extends Shape {
+	
+	private double radius;	
+
+	public Sphere(double radius) {
+		this.radius = radius;
+	}
+
+	@Override
+	double surface_area() {
+		return (4*Math.PI*Math.pow(radius, 2));
+	}
+
+	@Override
+	double volume() {
+		return (4/3*Math.PI*Math.pow(radius, 3));
+	}
+
+}
