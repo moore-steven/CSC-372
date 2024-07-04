f4ebf2d64eec7853649b1bde521c8d0548d170b9
moore-steven <steven.moore@csuglobal.edu>
2024-07-04 14:16:00 -0500

    Completed ShapeArray class

---
+++ CSC372/src/module4/ShapeArray.java
@@ -0,0 +1,26 @@
+package module4;
+
+import java.util.ArrayList;
+
+public class ShapeArray {
+
+
+	public static void main(String[] args) {
+		Sphere sphere1 = new Sphere(3.5);
+		Cylinder cylinder1 = new Cylinder(3.5,10);
+		Cone cone1 = new Cone(3.5,10);
+		
+		ArrayList<Shape> ShapeArray = new ArrayList<Shape>();
+		
+		ShapeArray.add(sphere1);
+		ShapeArray.add(cylinder1);
+		ShapeArray.add(cone1);
+		
+		for (Shape shape : ShapeArray) {
+			 
+	         System.out.println(shape.toString());
+	      }
+
+	}
+
+}
