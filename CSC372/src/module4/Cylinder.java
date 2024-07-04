50c0ff7ce57c6f20233eedd58e4ed235603fd0e5
moore-steven <steven.moore@csuglobal.edu>
2024-07-04 13:04:53 -0500

    Completed Cylinder class

---
+++ CSC372/src/module4/Cylinder.java
@@ -0,0 +1,26 @@
+package module4;
+
+public class Cylinder extends Shape {
+	private double radius;
+	private double height;
+
+	public Cylinder(double radius, double height) {
+		this.radius = radius;
+		this.height = height;
+	}
+
+	@Override
+	double surface_area() {
+		return ((2*Math.PI*radius*height)+(2*Math.PI*Math.pow(radius, 2)));
+	}
+
+	@Override
+	double volume() {
+		return Math.PI*Math.pow(radius, 2)*height;
+	}
+	
+	public String toString() {
+		return "Volume: " + volume() + " Surface Area: " + surface_area();
+	}
+
+}
