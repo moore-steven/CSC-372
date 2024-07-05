78961be4ce35155d49506c8124b50c74d1c2625d
moore-steven <steven.moore@csuglobal.edu>
2024-07-05 08:44:55 -0500

    Comments for Cylinder class

--- CSC372/src/module4/Cylinder.java
+++ CSC372/src/module4/Cylinder.java
@@ -1,23 +1,33 @@
 package module4;
 
+//Creation of Cylinder class that extends abstract Shape class
+
 public class Cylinder extends Shape {
 	private double radius;
 	private double height;
 
+	// Constructor that accepts radius and height variables
+	
 	public Cylinder(double radius, double height) {
 		this.radius = radius;
 		this.height = height;
 	}
 
+	// Surface area method for this shape
+	
 	@Override
 	double surface_area() {
 		return ((2*Math.PI*radius*height)+(2*Math.PI*Math.pow(radius, 2)));
 	}
 
+	// Volume method for this shape
+	
 	@Override
 	double volume() {
 		return Math.PI*Math.pow(radius, 2)*height;
 	}
+	
+	// toString method to output calculated volume and surface area
 	
 	public String toString() {
 		return "Cylinder Volume: " + volume() + " - Cylinder Surface Area: " + surface_area();
