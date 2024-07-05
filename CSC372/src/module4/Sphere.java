05f02949534572c8081e477bf765d82252aa9a64
moore-steven <steven.moore@csuglobal.edu>
2024-07-05 08:46:28 -0500

    Added comments to Sphere class

--- CSC372/src/module4/Sphere.java
+++ CSC372/src/module4/Sphere.java
@@ -1,23 +1,32 @@
 package module4;
 
+// Creation of Sphere class that extends abstract Shape class
 
 public class Sphere extends Shape {
 	
 	private double radius;	
 
+	// Constructor that accepts radius variable
+	
 	public Sphere(double radius) {
 		this.radius = radius;
 	}
 
+	// Surface area method for this shape
+	
 	@Override
 	double surface_area() {
 		return (4*Math.PI*Math.pow(radius, 2));
 	}
 
+	// Volume method for this shape
+	
 	@Override	
 	double volume() {
 		return (4/3*Math.PI*Math.pow(radius, 3));
 	}
+	
+	// toString method to output calculated volume and surface area
 	
 	public String toString() {
 		return "Sphere Volume: " + volume() + " - Sphere Surface Area: " + surface_area();
