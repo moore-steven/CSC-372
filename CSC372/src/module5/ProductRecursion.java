60c30d7525de8971cca72df028a5135df1950252
moore-steven <steven.moore@csuglobal.edu>
2024-07-13 15:59:48 -0500

    Creation of input process

--- CSC372/src/module5/ProductRecursion.java
+++ CSC372/src/module5/ProductRecursion.java
@@ -1,14 +1,23 @@
 package module5;
 
-public class ProductRecursion {
+import java.util.Scanner;
 
+public class ProductRecursion {
+	private static int[] input = new int[5];
 	public ProductRecursion() {
 		// TODO Auto-generated constructor stub
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Enter 5 numbers");
+		
+		for (int i=0; i < 5; i++) {
+			System.out.println("Number " + (i + 1) + ": ");
+			Scanner s = new Scanner(System.in);
+			input[i] = s.nextInt();
+		}
+		
+		System.out.println(input[1]);
 	}
 
 }
