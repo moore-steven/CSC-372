a795df9ce24cb9301ad62a8116f47abe2d030217
moore-steven <steven.moore@csuglobal.edu>
2024-07-21 11:29:45 -0500

    Create Name Comparator

--- CSC372/src/module6/ArrayListClass.java
+++ CSC372/src/module6/ArrayListClass.java
@@ -9,8 +9,15 @@ public class ArrayListClass {
 		ArrayList<Student> studentlist = new ArrayList<> ();
 		
 		studentlist.add(new Student(1,"Bill","Water Street"));
-		
-		System.out.println(studentlist.get(0).name);
+		studentlist.add(new Student(2,"Penelope","Brink Street"));
+		studentlist.add(new Student(3,"Sarah","Fall Drive"));
+		studentlist.add(new Student(4,"Danny","Main Street"));
+		studentlist.add(new Student(5,"Christy","5th Street"));
+		studentlist.add(new Student(6,"Samuel","Wall Street"));
+		studentlist.add(new Student(7,"John","Park Avenue"));
+		studentlist.add(new Student(8,"Matt","Old 60 Lane"));
+		studentlist.add(new Student(9,"Corey","Upalika Street"));
+		studentlist.add(new Student(10,"Jaime","Pine Street"));
 
 	}
 
